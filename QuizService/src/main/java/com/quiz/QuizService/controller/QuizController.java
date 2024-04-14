c204af7b829949bb6c61eca9285ab548a649f8f4
Purvesh Krishnani <purvesh152001@gmail.com>
2024-04-15 01:38:09 +0530

    .

--- QuizService/src/main/java/com/quiz/QuizService/controller/QuizController.java
+++ QuizService/src/main/java/com/quiz/QuizService/controller/QuizController.java
@@ -1,2 +1,42 @@
-package com.quiz.QuizService.controller;public class QuizController {
+package com.quiz.QuizService.controller;
+
+
+import com.quiz.QuizService.entity.Quiz;
+import com.quiz.QuizService.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/quiz")
+public class QuizController {
+
+
+    @Autowired
+    private QuizService quizService;
+
+    public QuizController(QuizService quizService) {
+        this.quizService = quizService;
+    }
+
+    @PostMapping("/create")
+    public Quiz create(@RequestBody Quiz quiz)
+    {
+        return quizService.add(quiz);
+    }
+
+
+    @GetMapping("/getall")
+    public List<Quiz> get() {
+        return quizService.get();
+    }
+
+    @GetMapping("/{:id}")
+    public Quiz getOne(@PathVariable Long id)
+    {
+        return quizService.get(id);
+    }
+
+
 }
